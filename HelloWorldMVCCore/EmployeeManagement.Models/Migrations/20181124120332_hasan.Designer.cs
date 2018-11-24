0328901ee70422188e6703962d2b93f9544e7b81
hmuzzal <hmuzzal.diu@gmail.com>
2018-11-24 18:35:45 +0600

    commitedByHasan

--- HelloWorldMVCCore/EmployeeManagement.Models/Migrations/20181124120332_hasan.Designer.cs
+++ HelloWorldMVCCore/EmployeeManagement.Models/Migrations/20181124120332_hasan.Designer.cs
@@ -3,14 +3,16 @@ using EmployeeManagement.Models.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManagemetApp.Migrations
 {
     [DbContext(typeof(EmployeeDbContext))]
-    partial class EmployeeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181124120332_hasan")]
+    partial class hasan
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HelloWorldMVCCore/EmployeeManagement.Models/Migrations/20181124120332_hasan.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagemetApp.Migrations
+{
+    public partial class hasan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
